264feadc89db4680b2b71fe344ecb0fe553fa319
Harsh shukla <hshukla1@mail.depaul.edu>
2017-11-21 19:29:13 +0000

    make it better

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
   get 'admin' => 'admin#index'
   
-  controller :sessions do
-    get 'login' => :new
+controller :sessions do
+    get  'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
 
+  resources :users
   resources :orders
-  root 'store#index', as: 'store_index'
   resources :line_items
   resources :carts
-  get 'store/index'
-  
-  resources :users
-  resources :products
-  #root 'products#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root 'products#index'
+  root 'store#index', as: 'store_index'
+
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
 end
